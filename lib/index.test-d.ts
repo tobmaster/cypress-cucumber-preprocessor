b7a8f36e38fbb2dab5e29d7b01640a70d1289b86
Jonas Amundsen <jonasba@gmail.com>
2022-08-20 12:55:35 +0200

    Replace dtslint with tsd
    This seems appropriate. I couldn't quite get dtslint to work without
    also validating types of dependencies, which is not quite what I want.

---
+++ lib/index.test-d.ts
@@ -0,0 +1,114 @@
+import { expectType } from "tsd";
+
+import messages from "@cucumber/messages";
+
+import "./";
+
+import {
+  Given,
+  When,
+  Then,
+  Step,
+  defineParameterType,
+  Before,
+  After,
+  DataTable,
+} from "../methods";
+
+Given("foo", function (foo, bar: number, baz: string) {
+  expectType<Mocha.Context>(this);
+  expectType<unknown>(foo);
+  expectType<number>(bar);
+  expectType<string>(baz);
+});
+
+Given(/foo/, function (foo, bar: number, baz: string) {
+  expectType<Mocha.Context>(this);
+  expectType<unknown>(foo);
+  expectType<number>(bar);
+  expectType<string>(baz);
+});
+
+When("foo", function (foo, bar: number, baz: string) {
+  expectType<Mocha.Context>(this);
+  expectType<unknown>(foo);
+  expectType<number>(bar);
+  expectType<string>(baz);
+});
+
+When(/foo/, function (foo, bar: number, baz: string) {
+  expectType<Mocha.Context>(this);
+  expectType<unknown>(foo);
+  expectType<number>(bar);
+  expectType<string>(baz);
+});
+
+Then("foo", function (foo, bar: number, baz: string) {
+  expectType<Mocha.Context>(this);
+  expectType<unknown>(foo);
+  expectType<number>(bar);
+  expectType<string>(baz);
+});
+
+Then(/foo/, function (foo, bar: number, baz: string) {
+  expectType<Mocha.Context>(this);
+  expectType<unknown>(foo);
+  expectType<number>(bar);
+  expectType<string>(baz);
+});
+
+declare const table: DataTable;
+
+Then("foo", function () {
+  // Step should consume Mocha.Context.
+  Step(this, "foo");
+});
+
+Then("foo", function () {
+  // Step should consume DataTable's.
+  Step(this, "foo", table);
+});
+
+Then("foo", function () {
+  // Step should consume doc strings.
+  Step(this, "foo", "bar");
+});
+
+defineParameterType({
+  name: "foo",
+  regexp: /foo/,
+  transformer(foo, bar, baz) {
+    expectType<Mocha.Context>(this);
+    expectType<string>(foo);
+    expectType<string>(bar);
+    expectType<string>(baz);
+  },
+});
+
+Before(function () {
+  expectType<Mocha.Context>(this);
+});
+
+Before({}, function () {
+  expectType<Mocha.Context>(this);
+});
+
+Before({ tags: "foo" }, function () {
+  expectType<Mocha.Context>(this);
+});
+
+After(function () {
+  expectType<Mocha.Context>(this);
+});
+
+After({}, function () {
+  expectType<Mocha.Context>(this);
+});
+
+After({ tags: "foo" }, function () {
+  expectType<Mocha.Context>(this);
+});
+
+expectType<messages.GherkinDocument>(window.testState.gherkinDocument);
+expectType<messages.Pickle[]>(window.testState.pickles);
+expectType<messages.Pickle>(window.testState.pickle);
--- types/testState.ts
+++
@@ -1,3 +0,0 @@
-window.testState.gherkinDocument; // $ExpectType GherkinDocument
-window.testState.pickles; // $ExpectType Pickle[]
-window.testState.pickle; // $ExpectType Pickle
